453efa862c87c27fe8ad556110ca3a54f129a558
Cheung Dan <dancheung80@gmail.com>
2015-04-22 02:10:06 +0800

    First commit

---
+++ app.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var config=require('./config');
+//set prommise as global
+GLOBAL.Q=require('q');
+
+//router setup
+var route= {
+    auth: require('./route/auth'),
+    tasks: require('./route/tasks')
+};
+
+var app = express();
+app.set('env',config.ENV);
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'www/app')));
+app.use('/auth',route.auth);
+app.use('/tasks',route.tasks);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.json('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({error:'Error occured`'});
+});
+
+module.exports = app;
